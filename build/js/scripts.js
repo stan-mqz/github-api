eedc385f77cb0a9d1c230a7e1b0b22beed259937
Stan <moscarstanley37@gmail.com>
2024-09-05 21:44:17 -0600

    added markup

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -1,1 +1,19 @@
-console.log(Hola);
+const form = document.querySelector('.form')
+
+
+
+form.addEventListener('submit', e => {
+
+    e.preventDefault();
+
+    const formData = new FormData(form);
+    console.log(formData.get('nombre'));
+
+})
+
+const fetchData = () => {
+
+
+    fetch();
+
+}
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,1 +1,12 @@
-console.log(Hola);
+const form = document.querySelector('.form')
+
+
+
+form.addEventListener('submit', e => {
+
+    e.preventDefault();
+    const formData = new formData(form);
+   console.log(formData.get('nombre'));
+    
+})
+
