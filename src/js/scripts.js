ca3de15ae4b43ce728032804bd0c8ee9c35ab948
stan-mqz <moscarstanley37@gmail.com>
2024-10-22 08:29:24 -0600

    Added Form validaiton, now it disables the button if input is empty

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,92 +1,76 @@
-const form = document.querySelector('.form')
+const form = document.querySelector(".form");
+const input = document.querySelector(".form__text");
+const button = document.querySelector(".form__submit");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const name = input.value;
+  fetchData(name);
+  form.reset();
+});
 
 
-
-form.addEventListener('submit', e => {
-
-    e.preventDefault();
-    const formData = new FormData(form);
-    const getName = formData.get('nombre');
-    fetchData(getName)
-    form.reset();
+input.addEventListener('input', (e) => {
+    if (e.target.value === '') {
+        button.disabled = true
+    } else {
+        button.disabled = false
+    }
 })
 
-
 const fetchData = (name) => {
+  fetch(`https://api.github.com/users/${name}`)
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error("Not Found");
+      }
 
+      return res.json();
+    })
 
-    fetch(`https://api.github.com/users/${name}`)
+    .then((data) => {
+      const { id, login, followers, avatar_url, public_repos } = data;
+      addELements(id, login, followers, avatar_url, public_repos);
+    })
 
-        .then(res => {
+    .catch((err) => {
+      throw new Error(err);
+    });
+};
 
-            if (!res.ok) {
+const addELements = (id, login, followers, avatar_url, public_repos) => {
+  const body = document.querySelector("body");
+  const info = document.createElement("DIV");
+  const infoContent = document.createElement("DIV");
+  const img = document.createElement("IMG");
+  const p = document.createElement("P");
+  const ul = document.createElement("UL");
+  const idUser = document.createElement("LI");
+  const followersUser = document.createElement("LI");
+  const repositoriesUser = document.createElement("LI");
 
-                throw new Error("Not Found");
+  info.classList.add("info");
+  infoContent.classList.add("info__content");
+  img.classList.add("info__img");
+  p.classList.add("info__name");
+  ul.classList.add("info__list");
+  idUser.classList.add("info__list-item");
+  followersUser.classList.add("info__list-item");
+  repositoriesUser.classList.add("info__list-item");
 
-            }
+  img.src = avatar_url;
+  p.innerText = login;
+  idUser.innerText = `ID: ${id}`;
+  followersUser.innerText = `Seguidores: ${followers}`;
+  repositoriesUser.innerText = `Repositorios públicos: ${public_repos}`;
 
-            return res.json()
-        })
+  body.appendChild(info);
+  info.appendChild(infoContent);
+  infoContent.appendChild(img);
+  infoContent.appendChild(p);
+  info.appendChild(ul);
+  ul.appendChild(idUser);
+  ul.appendChild(followersUser);
+  ul.appendChild(repositoriesUser);
 
-        .then(data => {
-
-            const { id, login, followers, avatar_url, public_repos } = data;
-            addELements(id, login, followers, avatar_url, public_repos)
-        })
-
-        .catch(err => {
-
-            throw new Error(err);
-        })
-
-}
-
-
-const addELements = (id, login, followers, avatar_url, public_repos ) => {
-    const body = document.querySelector('body');
-    const info = document.createElement('DIV');
-    const infoContent = document.createElement('DIV');
-    const img = document.createElement('IMG');
-    const p = document.createElement('P');
-    const ul = document.createElement('UL');
-    const idUser = document.createElement('LI');
-    const followersUser = document.createElement('LI');
-    const repositoriesUser = document.createElement('LI');
-    
-    info.classList.add('info');
-    infoContent.classList.add('info__content');
-    img.classList.add('info__img');
-    p.classList.add('info__name');
-    ul.classList.add('info__list');
-    idUser.classList.add('info__list-item');
-    followersUser.classList.add('info__list-item');
-    repositoriesUser.classList.add('info__list-item');
-    
-    img.src = avatar_url;
-    p.innerText = login;
-    idUser.innerText = `ID: ${id}`;
-    followersUser.innerText = `Seguidores: ${followers}`;
-    repositoriesUser.innerText = `Repositorios públicos: ${public_repos}`;
-    
-    body.appendChild(info);
-    info.appendChild(infoContent);
-    infoContent.appendChild(img);
-    infoContent.appendChild(p);
-    info.appendChild(ul);
-    ul.appendChild(idUser);
-    ul.appendChild(followersUser);
-    ul.appendChild(repositoriesUser);
-    
-
-    // setTimeout(() => {
-        
-    //     info.remove(infoContent);
-    //     infoContent.remove(img);
-    //     infoContent.remove(p);
-    //     infoContent.remove(ul);
-    //     ul.remove(idUser);
-    //     ul.remove(followersUser);
-    //     ul.remove(repositoriesUser);
-
-    // }, 5000);
-}
+};
